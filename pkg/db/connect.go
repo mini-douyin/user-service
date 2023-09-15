7739155fa39f86268f2f6e090a40650abec8fec3
C Liu <ghovik@gmail.com>
2023-09-15 21:41:05 +0800

    added db conntion

---
+++ pkg/db/connect.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+	"user-service/models"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	migrateModels()
+}
+
+func migrateModels() {
+	err := DB.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Error migrating: %v", err)
+	}
+}
