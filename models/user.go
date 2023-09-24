5c4c367f09e35929d29f605f428351c052cbc2d9
C Liu <ghovik@gmail.com>
2023-09-24 13:46:15 +0800

    Added user profile

--- models/user.go
+++ models/user.go
@@ -6,9 +6,11 @@ import (
 )
 
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Email    string `gorm:"uniqueIndex;not null" binding:"required,email,max=100"`
-	Password string `gorm:"not null" binding:"required,min=6,max=50"`
+	gorm.Model
+	ID       uint        `gorm:"primaryKey" json:"id"`
+	Email    string      `gorm:"uniqueIndex;not null" binding:"required,email,max=100" json:"email"`
+	Password string      `gorm:"not null" binding:"required,min=6,max=50" json:"password"`
+	Profile  UserProfile `gorm:"foreignKey:UserId" json:"profile"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
@@ -20,7 +22,28 @@ func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
 	return
 }
 
+func (user *User) AfterCreate(tx *gorm.DB) (err error) {
+	userProfile := UserProfile{UserId: user.ID}
+	if err = tx.Create(&userProfile).Error; err != nil {
+		return err
+	}
+	return
+}
+
 func (user *User) CheckPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	return err == nil
+}
+
+type UserProfile struct {
+	gorm.Model
+	UserId         uint   `gorm:"unique" json:"user_id"`
+	Avatar         string `json:"avatar"`
+	Background     string `json:"background"`
+	Signature      string `json:"signature"`
+	FollowingCount int    `gorm:"default:0" json:"following_count"`
+	FollowerCount  int    `gorm:"default:0" json:"follower_count"`
+	LikesGiven     int    `gorm:"default:0" json:"likes_given"`
+	LikesReceived  int    `gorm:"default:0" json:"likes_received"`
+	VideoCount     int    `gorm:"default:0" json:"video_count"`
 }
